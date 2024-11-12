dea1ad5b48c815561a4cc8c0ceff15059bfbc66d
Carlos Ribeiro <carlos.ribeiro@fortics.com.br>
2024-11-11 22:17:41 -0300

    initial settings

---
+++ PlannerFinancial/src/App.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import { Box } from "@mui/material";
+
+export default function App() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100%",
+        p: 0,
+        background: "#000000",
+      }}
+    ></Box>
+  );
+}
